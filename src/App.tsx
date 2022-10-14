809333285b533206d35e070bababaa53d82e25cf
Ana Santos <anabia200113@gmail.com>
2022-10-14 10:37:58 -0300

    principal

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,16 @@ import CadastroUsuario from './components/pages/cadastroUsuario/CadastroUsuario'
 import Home from './components/pages/Home/Home';
 import Login from './components/pages/Login/Login';
 //import './App.css';//
+import Principal from './components/Principal/Principal'
+
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-        <div style={{ minHeight: '100vh'}}>
+        <div style={{ minHeight: '80vh'}}>
           <Routes>
-            {/* <Route path='/' element={ <Login /> } /> */}
+            <Route path='/' element={ <Principal /> } />
             <Route path='/login' element={ <Login /> } />
             <Route path='/cadastrousuario' element={ <CadastroUsuario /> } />
             <Route path='/home' element={ <Home /> } />
---
+++ src/components/pages/Principal/Principal.tsx
@@ -0,0 +1,115 @@
+import './Principal.css'
+import React from 'react'
+import img from '../../components/Principal/imagem/bss.svg'  // importando a imagem p/ ser chamada como variável lá embaixo - aí dá certo p/ fazer o deploy
+
+
+function Principal() {
+    return (
+        <div className="App">
+        {/* cabeçalho*/}
+
+            <header className="header-menu">
+                <div className='title'>
+                    <h1>SolutiON</h1>
+                </div>
+
+                <nav>
+                    <ul className="lista-menu">
+                        <li>
+                            <a href="#sobre">Sobre</a>
+                        </li>
+                        <li>
+                            <a href="#iniciativas">Iniciativas</a>
+                        </li>
+                        <li>
+                            <a href="#servicos">Serviços</a>
+                        </li>
+                        <li>
+                            <a href="#alcance">Alcance</a>
+                        </li>
+                        <li>
+                            <a href="#contato">Contato</a>
+                        </li>
+                        <li>
+                            <a href="#login" className='login'>Login</a>
+                        </li>
+
+                    </ul>
+                </nav>
+            </header>
+
+            {/* página principal  */}
+            <main>
+
+                {/* metade da tela p/ o texto */}
+                <aside>
+                    <h2>Soluti<span>ON</span></h2>
+                    <h3>Soluções empresariais</h3>
+                    <p>conectando alguma coisa e blablablaaaaaaaaaaaaaaa</p>
+                    <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+                    <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+
+
+                    <input type="submit" value="Cadastre a sua empresa" className="enviar" />
+                </aside>
+
+                <article>
+                    <img src={img} alt="COLOCAR ALGUMA DESCRIÇÃO MINIMAMENTE COERENTE" />
+                </article>
+                
+                <section>
+                    <article id="sobre">
+
+                    </article>
+                </section>
+                        
+
+                <section>
+                    <article id="iniciativas">
+
+                    </article>
+                </section>
+
+
+                <section>
+                    <article id="servicos">
+
+                    </article>
+                </section>
+
+
+                <section>
+                    <article id="alcance">
+
+                    </article>
+                </section>
+
+
+                <section>
+                    <article id="contato">
+
+                    </article>
+                </section>
+
+
+                <section>
+                    <article id="login">
+
+                    </article>
+                </section>
+
+            </main>
+
+
+
+    
+            
+                
+    
+    
+        </div>
+    )
+}
+
+
+export default Principal
