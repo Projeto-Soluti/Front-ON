f60f11261f59c63a972bbdc32be4b5e501a457a6
Marianadsm <mariana.dsm@hotmail.com>
2022-10-16 20:30:15 -0300

    Footer componente principal

--- src/components/pages/Principal/Principal.tsx
+++ src/components/pages/Principal/Principal.tsx
@@ -1,13 +1,13 @@
 import './Principal.css'
 import React from 'react'
 import img from '../Principal/imagem/bss.svg' // importando a imagem p/ ser chamada como variável lá embaixo - aí dá certo p/ fazer o deploy
-
-
+import { Grid } from '@material-ui/core'
+import { Box } from '@mui/material'
 
 function Principal() {
     return (
         <div className="App">
-        {/* cabeçalho*/}
+            {/* cabeçalho*/}
 
             <header className="header-menu">
                 <div className='title'>
@@ -57,13 +57,13 @@ function Principal() {
                 <article>
                     <img src={img} alt="COLOCAR ALGUMA DESCRIÇÃO MINIMAMENTE COERENTE" />
                 </article>
-                
+
                 <section>
                     <article id="sobre">
 
                     </article>
                 </section>
-                        
+
 
                 <section>
                     <article id="iniciativas">
@@ -98,16 +98,80 @@ function Principal() {
 
                     </article>
                 </section>
-
             </main>
 
+            <section className="footer">
+                <footer>
+                    <div className="itensFooter">
+                        <div>
+                            <div className="footer-left">
+                                <h3 className="soluti" >SOLUTI <span className="on" > ON </span> </h3>
+
+                                <div className="footer-links" >
+                                    <a href="#">Home</a>
+                                     | 
+                                    <a href="#">Sobre Nós</a>
+                                     | 
+                                    <a href="#">Contato</a>
+                                     | 
+                                    <a href="#">Solutioners</a>
+                                </div>
+
+                                <p className="footer-company-name">Copyright © 2022 <strong>Soluti ON</strong> All rights reserved</p>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="footer-center">
+                                <div className="local">
+                                    <i className="local"></i>
+                                    <p>Brasil</p>
+                                </div>
+
+                                <div className="telefone">
+                                    <i className="telefone"></i>
+                                    <p>+55 11 1234-5678</p>
+                                </div>
+                                <div className="email">
+                                    <i className="email"></i>
+                                    <p><a href="mailto:onsoluti7@gmail.com">onsoluti7@gmail.com</a></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="footer-right">
+                                <p className="footer-company-about">
+                                    <span>Sobre nós</span>
+                                    A <strong >SOLUTION</strong> é uma rede estratégica e ágil para pequenos negócios, visando fortalecer o comércio nacional no rol do mercado financeiro até 2030.
+                                </p>
+                                <div className="footer-icons">
+                                    <a href="https://www.github.com/projeto-soluti" target="_blank">
+                                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="github" />
+                                    </a>
+
+                                    <a href="#" target="_blank">
+                                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/facebook/facebook-original.svg" alt="facebook" />
+                                    </a>
+
+                                    <a href="#" target="_blank">
+                                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" alt="linkedin" />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </footer>
+            </section>
 
 
-    
-            
-                
-    
-    
+
+
+
+
+
+
+
+
         </div>
     )
 }
