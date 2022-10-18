50136bc4014609f91f7b238b6870b5a076fb39df
Pedro Henrique Camargo <104800947+Pddro1@users.noreply.github.com>
2022-10-18 16:27:13 -0300

    mudanças esteticas

--- src/App.tsx
+++ src/App.tsx
@@ -28,8 +28,9 @@ function App() {
         <BrowserRouter>
           <Navbar />
             <div style={{ minHeight: '80vh'}}>
+              
               <Routes>
-
+                
                 <Route path='/' element={ <Principal /> } />
                 <Route path='/login' element={ <Login /> } />
                 <Route path='/cadastrousuario' element={ <CadastroUsuario /> } />
@@ -43,9 +44,7 @@ function App() {
                 <Route path='/editarPost' element={ <CadastroPostagem /> } />
                 <Route path='/editarPost/:id' element={ <CadastroPostagem /> } />
                 <Route path='/apagarPost/:id' element={ <DeletarPostagem /> } />
-
-              
-
+          
               </Routes>
             </div>
           <Footer />
