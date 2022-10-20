d29769072d8d2486b65239604d5cb2b28a3ddd06
Thais Melim <thaiscristinarisonho@gmail.com>
2022-10-20 09:55:19 -0300

    Atualizando Carousel

--- src/components/pages/Principal/Parceiros/Parceiros.tsx
+++ src/components/pages/Principal/Parceiros/Parceiros.tsx
@@ -1,32 +1,75 @@
-import './Parceiros.css'
-import AliceCarousel from 'react-alice-carousel'
-import 'react-alice-carousel/lib/alice-carousel.css'
+import './Parceiros.css';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import { Container } from '@mui/material';
 
-function Parceiros(){
+function Parceiros() {
+  const responsive = {
+    0: { items: 1 },
+    500: { items: 3 },
+    1024: { items: 4 },
+  };
 
-    const responsive = {
-        0: { items: 1 },
-        500: { items: 3 },
-        1024: { items: 4 },
-      };
+  const items = [
+    <div className="itemBanco">
+      <div className="banco">
+        <a href="https://www.sicoob.com.br/">
+          <img
+            src="http://acian.com.br/wp-content/uploads/2019/08/logo-sicoob.jpg"
+            alt="Sicoob"
+          />
+        </a>
+      </div>
+    </div>,
 
-      const items = [
-        <div className="item">
-          <div className="banco" >
-          <a href="https://www.sicoob.com.br/"><img src="http://acian.com.br/wp-content/uploads/2019/08/logo-sicoob.jpg"  alt="Sicoob"/></a>
-          <a href="https://www.bndes.gov.br/"><img src="https://www.amstedmaxion.com.br/wp-content/uploads/2022/07/BNDES-logo.jpg" alt="Bndes"/></a>
-          <a href="https://www.daycoval.com.br/"><img src="https://www.sicredi.com.br/media/coop/filer_public/2020/05/13/logo_para__noticias_-_site.jpg" alt="Sicredi"/></a>
-          <a href="https://www.bb.com.br/"><img src="https://i0.wp.com/www.spportoes.com.br/wp-content/uploads/2016/06/logo-banco-do-brasil.jpg" alt="Banco Brasil"/></a>
-          <a href="https://www.daycoval.com.br/"><img src="https://q3m9s2m7.rocketcdn.me/wp-content/uploads/2022/02/banco-daycoval-consignado.png" alt="Banco Daycoval"/></a>
-          </div>
-          </div>
-      ]
-      
-    return(
-      <div>
+    <div className="itemBanco">
+      <div className="banco">
+        <a href="https://www.bndes.gov.br/">
+          <img
+            src="https://www.amstedmaxion.com.br/wp-content/uploads/2022/07/BNDES-logo.jpg"
+            alt="Bndes"
+          />
+        </a>
+      </div>
+    </div>,
 
+    <div className="itemBanco">
+      <div className="banco">
+        <a href="https://www.daycoval.com.br/">
+          <img
+            src="https://www.sicredi.com.br/media/coop/filer_public/2020/05/13/logo_para__noticias_-_site.jpg"
+            alt="Sicredi"
+          />
+        </a>
+      </div>
+    </div>,
+    <div className="itemBanco">
+      <div className="banco">
+        <a href="https://www.bb.com.br/">
+          <img
+            src="https://i0.wp.com/www.spportoes.com.br/wp-content/uploads/2016/06/logo-banco-do-brasil.jpg"
+            alt="Banco Brasil"
+          />
+        </a>
+      </div>
+    </div>,
+    <div className="itemBanco">
+      <div className="banco">
+        <a href="https://www.daycoval.com.br/">
+          <img
+            src="https://q3m9s2m7.rocketcdn.me/wp-content/uploads/2022/02/banco-daycoval-consignado.png"
+            alt="Banco Daycoval"
+          />
+        </a>
+      </div>
+    </div>,
+  ];
+
+  return (
+    <div className="App">
       <h1>Conheça nossos parceiros </h1>
-      
+
+      <Container>
       <AliceCarousel
         mouseTracking
         disableButtonsControls={false}
@@ -35,13 +78,15 @@ function Parceiros(){
         controlsStrategy="alternate"
         infinite
         autoPlay
-        autoPlayStrategy="none"
+        disableDotsControls={true}
+        autoPlayStrategy="all"
         autoPlayInterval={800}
         animationDuration={800}
         animationType="fadeout"
-        />
+      />
+      </Container>
     </div>
-      )
+  );
 }
 
 export default Parceiros;
