8d4166496818092e5604959fe13509628edd4e59
Thais Melim <thaiscristinarisonho@gmail.com>
2022-10-20 09:18:08 -0300

    Carousel

--- src/components/pages/Principal/Parceiros/Parceiros.tsx
+++ src/components/pages/Principal/Parceiros/Parceiros.tsx
@@ -12,10 +12,13 @@ function Parceiros(){
 
       const items = [
         <div className="item">
-          <div className="banco"></div>
-          <a href="https://www.sicoob.com.br/"><img src="https://cdn.cookielaw.org/logos/2120f93f-21cc-4b68-9b49-e6d689047dd4/abe00d3c-1e31-48f7-9f85-876199b6d687/1e8751c1-22ab-4706-afdb-4d16a50d8cb5/Sicoob_fundo_branco.png"  alt="Sicoob"/></a>
-          <a href="https://www.bndes.gov.br/"><img src="https://www.gigasecurity.com.br/imagens/logo-bndes.png" alt="Bndes"/></a>
-          <a href="https://www.bb.com.br/"><img src="https://res.cloudinary.com/compara/image/upload/v1638900941/leadgen/divisions/logos/Emprestimo-BR/banco-do-brasil_idc6uo.png" alt="Banco Brasil"/></a>
+          <div className="banco" >
+          <a href="https://www.sicoob.com.br/"><img src="http://acian.com.br/wp-content/uploads/2019/08/logo-sicoob.jpg"  alt="Sicoob"/></a>
+          <a href="https://www.bndes.gov.br/"><img src="https://www.amstedmaxion.com.br/wp-content/uploads/2022/07/BNDES-logo.jpg" alt="Bndes"/></a>
+          <a href="https://www.daycoval.com.br/"><img src="https://www.sicredi.com.br/media/coop/filer_public/2020/05/13/logo_para__noticias_-_site.jpg" alt="Sicredi"/></a>
+          <a href="https://www.bb.com.br/"><img src="https://i0.wp.com/www.spportoes.com.br/wp-content/uploads/2016/06/logo-banco-do-brasil.jpg" alt="Banco Brasil"/></a>
+          <a href="https://www.daycoval.com.br/"><img src="https://q3m9s2m7.rocketcdn.me/wp-content/uploads/2022/02/banco-daycoval-consignado.png" alt="Banco Daycoval"/></a>
+          </div>
           </div>
       ]
       
@@ -33,10 +36,10 @@ function Parceiros(){
         infinite
         autoPlay
         autoPlayStrategy="none"
-        autoPlayInterval={1000}
-        animationDuration={1000}
+        autoPlayInterval={800}
+        animationDuration={800}
         animationType="fadeout"
-        disableDotsControls/>
+        />
     </div>
       )
 }
